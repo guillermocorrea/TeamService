d62e125b85493d95964c3d2860e0de7cf7b4158e
Luis Guillermo Correa <guillermo.correa.99@gmail.com>
2018-02-12 20:53:10 -0500

    Initial setup

---
+++ TeamService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
