f30a271c1a4a87d0a91c26b86e9af677a0ccd084
Luis Guillermo Correa <guillermo.correa.99@gmail.com>
2018-02-12 21:59:43 -0500

    Added build script, code coverage

---
+++ build.sh
@@ -0,0 +1,28 @@
+dotnet restore
+dotnet build
+
+cd TeamService.Tools
+
+# Instrument assemblies inside 'test' folder to detect hits for source files inside 'src' folder
+dotnet minicover instrument --workdir ../ --assemblies TeamService.Tests/**/bin/**/*.dll --sources TeamService/**/*.cs 
+
+# Reset hits count in case minicover was run for this project
+dotnet minicover reset
+
+cd ..
+
+for project in TeamService.Tests/*.csproj; do dotnet test --no-build $project; done
+
+cd TeamService.Tools
+
+# Uninstrument assemblies, it's important if you're going to publish or deploy build outputs
+dotnet minicover uninstrument --workdir ../
+
+# Create html reports inside folder coverage-html
+dotnet minicover htmlreport --workdir ../ --threshold 75
+
+# Print console report
+# This command returns failure if the coverage is lower than the threshold
+dotnet minicover report --workdir ../ --threshold 75
+
+cd ..
